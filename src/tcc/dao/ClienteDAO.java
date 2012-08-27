7791833f671620ca02f3f888ea4bbd491d90cd97
Alberson Melo <alberson@alberson.local>
2012-08-27 14:52:46 -0300

    Revert "Revert "Refatoracao do ClienteDAO e adição do ClienteController""
    This reverts commit d4df51a7eb43dd645deed70766a56796921082a2.

--- src/tcc/dao/ClienteDAO.java
+++ src/tcc/dao/ClienteDAO.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -59,3 +60,77 @@ public class ClienteDAO {
     }
     
 }
+=======
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcc.dao;
+
+import java.awt.Component;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import tcc.model.Cliente;
+
+/**
+ *
+ * @author alberson
+ */
+public class ClienteDAO {
+    
+    private static final Logger log = Logger.getLogger(ClienteDAO.class.getName());
+    private Session session;
+
+    public ClienteDAO(Session session) {
+        this.session = session;
+    }
+    
+    
+    public void adiciona(Cliente cliente, Component c) {
+        Transaction trx = session.beginTransaction();
+        trx.begin();
+        try {
+            session.save(cliente);
+            trx.commit();
+            log.log(Level.FINE, "Cliente inserido com sucesso");
+            JOptionPane.showMessageDialog(c, "Cliente inserido com sucesso", null, JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (HibernateException e) {
+            trx.rollback();
+            log.log(Level.SEVERE, "Cliente não inserido", e);
+            JOptionPane.showMessageDialog(c, "Não", null, JOptionPane.ERROR);
+        } finally {
+            session.close();
+        }
+    }
+    
+    public void adiciona(Cliente cliente){
+        Transaction trx = session.beginTransaction();
+        trx.begin();
+        try {
+            session.save(cliente);
+            trx.commit();
+            log.log(Level.FINE, "Cliente inserido com sucesso");
+        }
+        catch (HibernateException e) {
+            trx.rollback();
+            log.log(Level.SEVERE, "Cliente não inserido", e);
+        } finally {
+            session.close();
+        }
+    }
+    
+
+    public List<Cliente> lista(){
+        Criteria ct = session.createCriteria(Cliente.class);
+        return (List<Cliente>) ct.list();
+    }
+    
+}
+>>>>>>> Refatoracao do ClienteDAO e adição do ClienteController
