ec73510bd41fb06d902db3bd0842f909e86b855b
Alberson Melo <alberson@alberson.local>
2012-08-29 20:29:49 -0300

    Validacao de Login

--- src/tcc/ui/Login.java
+++ src/tcc/ui/Login.java
@@ -110,7 +110,10 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        validateLogin();
+        if (validateLogin()) {
+            this.setVisible(true);
+            new Principal().setVisible(true);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
